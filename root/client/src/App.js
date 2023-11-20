112bdc72b166d5a9438361e42bd236d9e8fb80af
Vichenry2002 <victor.henry@mail.mcgill.ca>
2023-11-20 17:47:57 -0500

    landing page

--- root/client/src/App.js
+++ root/client/src/App.js
@@ -1,16 +1,18 @@
+// App.js
 import React from "react";
-// We use Route in order to define the different routes of our application
 import { Route, Routes } from "react-router-dom";
-// We import all the components we need in our app
-//import RecordList from "./components/recordList";
+import LandingPage from "./components/Landing";
 import Create from "./components/Register";
+
 const App = () => {
     return (
         <div>
             <Routes>
+                <Route path="/" element={<LandingPage />} />
                 <Route path="/create" element={<Create />} />
             </Routes>
         </div>
     );
 };
+
 export default App;
