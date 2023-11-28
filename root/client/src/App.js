95e147d43c0550c4a9d7a19ff36b457894d4860d
willi <williamhuang24@gmail.com>
2023-11-28 18:49:51 -0500

    discussion board template

--- root/client/src/App.js
+++ root/client/src/App.js
@@ -1,15 +1,17 @@
 // App.js
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import LandingPage from "./components/Landing";
+import Landing2Page from "./components/Landing";
 import Register from "./components/Register";
+import Discussion from "./components/Discussion";
 
 const App = () => {
     return (
         <div>
             <Routes>
-                <Route path="/" element={<LandingPage />} />
+                <Route path="/" element={<Landing2Page />} />
                 <Route path="/register" element={<Register />} />
+                <Route path="/discussion" element={<Discussion />} />
             </Routes>
         </div>
     );
