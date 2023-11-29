4d1c0ea6778de50500a41275c53511ae9071940a
jjcs2000 <joshuatjiang@hotmail.com>
2023-11-29 13:55:29 -0500

    Update discussions.js

--- root/server/routes/discussions.js
+++ root/server/routes/discussions.js
@@ -55,7 +55,43 @@ discussionRoutes.route("/discussion/:discussionId/:userId/remove").post(async fu
 
 //Delete discussion
 
-//Update discussion (add users)
+//Get discussions by user_id
+userRoutes.route("/user/:username/discussions").get(async function (req, response) {
+    let db_connect = dbo.getDb();
+
+    try {
+        const result = await db_connect.collection("users").findOne({ name: req.params.username });
+
+        if (!result) {
+            response.status(404).send("User not found");
+            return;
+        }
+        response.json(result.discussions);
+    } catch (err) {
+        console.error("Error fetching user discussions:", err);
+        response.status(500).send("Internal Server Error");
+    }
+});
+
+
+// Endpoint to add a discussion to a user's discussions
+userRoutes.route("/user/:username/addDiscussion").put(function (req, res) {
+    let db_connect = dbo.getDb();
+    const discussionId = req.body.discussionId;
+    const title = req.body.discussionTitle; // Add title from the request body
+
+    db_connect.collection("users").updateOne(
+        { name: req.params.username }, // Use the username directly
+        { $push: { discussions: { discussionId, title } } }, // Add the tuple to the discussions array
+        function (err, result) {
+            if (err) {
+                res.status(500).send(err);
+            } else {
+                res.status(200).send("Discussion added successfully");
+            }
+        }
+    );
+});
 
 
 module.exports = discussionRoutes;
